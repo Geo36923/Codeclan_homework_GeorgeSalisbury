5134c8ad38f34ea9f0fbee0d6b57ac463bfbe6ef
George Salisbury <Geo36923@gmail.com>
2022-07-20 19:33:59 +0100

    Update to homework

---
+++ Week_05/Day_03/game_sales_app/app.R
@@ -0,0 +1,76 @@
+library(janitor)
+library(CodeClanData)
+library(tidyverse)
+library(assertr)
+library(shiny)
+library(bslib)
+game_sales
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  theme = bs_theme(version = 5, bootswatch = "vapor"),
+  titlePanel("Game Sales"),
+  tabsetPanel(
+    tabPanel(
+    "Genre Select",
+    selectInput(
+      inputId = "Genre",
+      label = "Choose your genre",
+      choices = c("Action", "Adventure", "Fighting", "Misc", "Platform", "Puzzle",
+                  "Racing", "Role-playing", "Shooter", "Simulation", "Sports", "Strategy")                  )
+    )
+  ),
+  tabPanel(
+  "Console select",
+  selectInput(
+    inputId = "Console",
+    label = "Choose your console",
+    choices = c("3DS", "DS", "GBA", "GC", "PC", "PS", "PS2", "PS3", "PS4", "PSP", "PSV", "Wii", "WiiU",
+                "XBOX", "X360", "XOne")
+  )
+  ),
+tabPanel("Plot",
+  plotOutput("Game Plot")
+
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  output$game_plot <- renderPlot(
+    game_sales %>%
+      filter(platform %in% c("Wii Sports",
+                         "Soviet Union",
+                          "Germany",
+                          "Italy",
+                          "Great Britain")) %>%
+      filter(genre == input$genre) %>%
+      filter(console == input$platform) %>%
+      mutate(overall_sales = sum(sales %in% platform)) %>% 
+      ggplot() +
+      aes(x = platform, y = overall_sales, fill = platform) +
+      geom_histogram(bars = 30, show.legend = "none") +
+      xlab("console")+
+      ylab("sales")+
+      scale_fill_manual(values = c("3DS" = "red", 
+                                   "DS" = "silver",  
+                                   "GBA" = "pink",
+                                   "GC" = "purple",
+                                   "PC" = "grey",
+                                   "PS" = "brown",
+                                   "PS2" = "blue",
+                                   "PS3" = "black",
+                                   "PS4" = "orange",
+                                   "PSV" = "white",
+                                   "Wii" = "cobalt",
+                                   "WiiU" = "vermilion",
+                                   "XB" = "light green",
+                                   "X360" = "dark green",
+                                   "XOne" = "dark red")) +
+      theme_minimal()
+  )
+  
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
