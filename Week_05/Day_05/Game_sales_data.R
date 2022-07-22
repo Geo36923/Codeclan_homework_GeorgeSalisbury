56f1778a5dc756a5ace38f0b80d1cf15f29c93f7
George Salisbury <Geo36923@gmail.com>
2022-07-22 16:58:42 +0100

    Update Game_sales_data.R

--- Week_05/Day_05/Game_sales_data.R
+++ Week_05/Day_05/Game_sales_data.R
@@ -10,12 +10,49 @@ game_sales <- CodeClanData::game_sales
 library(shiny)
 
 ui <- fluidPage(
-  fluidRow(
-    
-  )
-)
+  titlePanel("Game sales data"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "console",
+        label = "Which console?",
+        choices = unique(game_sales$platform)
+      )
+    )
+  ),
+  sidebarPanel(
+    selectInput(
+      inputId = "developer",
+      label = "Which developer?",
+      choices = unique(game_sales$developer)
+    )
+  ),
+  selectInput(
+    inputId = "release date",
+    label = "When did the game come out?",
+    choices = unique(game_sales$year_of_release)
+  ),
+  sliderInput(
+      inputId = "rating",
+      label = "What's the critic rating?",
+      value = 5.0, min = 0, max = 10
+    ),
+  mainPanel(
+    plotOutput(
+      "game_sales_plot"
+    )
+  ),
+game_sales$
 
 server <- function(input, output, session) {
+  output$game_sales_plot <- renderPlot({
+    game_sales %>% 
+      ggplot()+
+      aes(x = console, y = sales, fill = genre) +
+      
+  }
+    
+  )
   
 }
 
