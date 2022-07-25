d7254eeea0618cca08b2a744795106eb692f9a09
George Salisbury <Geo36923@gmail.com>
2022-07-25 13:49:15 +0100

    Update Game_sales_data.R

--- Week_05/Day_05/Game_sales_data.R
+++ Week_05/Day_05/Game_sales_data.R
@@ -4,6 +4,9 @@ library(tidyverse)
 library(bslib)
 library(shiny)
 library(shinyWidgets)
+library(ggplot2)
+library(DT)
+library(shinydashboard)
 
 game_sales <- CodeClanData::game_sales
 # time to start up the shiny app for calculating game_sales.  Thankfully, I did some work on this dataset earlier. ####
@@ -19,30 +22,28 @@ ui <- fluidPage(
         choices = unique(game_sales$platform)
       )
     )
-  ),
+  )
+  )
   sidebarPanel(
     selectInput(
       inputId = "developer",
       label = "Which developer?",
       choices = unique(game_sales$developer)
     )
-  ),
+  )
   selectInput(
     inputId = "release date",
     label = "When did the game come out?",
     choices = unique(game_sales$year_of_release)
-  ),
+  )
   sliderInput(
       inputId = "rating",
       label = "What's the critic rating?",
       value = 5.0, min = 0, max = 10
-    ),
-  mainPanel(
-    plotOutput(
-      "game_sales_plot"
     )
-  ),
-game_sales$
+  mainPanel(plotOutput("game_sales_plot")
+  )
+game_sales
 
 server <- function(input, output, session) {
   output$game_sales_plot <- renderPlot({
